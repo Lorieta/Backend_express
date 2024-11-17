d627746995d99a3d9c369217cd0df62048ac243e
Lorieta <johncarlo.lorieta@gmail.com>
2024-11-17 13:50:21 +0800

    ads

--- Server/app.js
+++ Server/app.js
@@ -1,20 +1,12 @@
 const express = require('express');
+const routes = require('./routes/user.js');
 
-const app = new express();
-const PORT= 3000;
+const app = express();
 
+const PORT = 3000; 
+app.use(express.json());
+app.use('/user', routes);
 
-
-
-app.get('/',(req,res)=>{
-res.send("hello")
-}); 
-
-app.listen(PORT,()=>{
-    console.log(PORT);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-app.delete('/del',(req,res)=>{
-    res.send("delte");
-})
-asd
